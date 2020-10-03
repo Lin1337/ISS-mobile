fbc2be2d4ffb154419dd7b1e652011db19a94d5d
Lin Diniz <gabrielvdiniz@gmail.com>
2020-10-02 22:18:12 -0300

    Map Working State

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-import React, {Component} from 'react';
-import {View, Text } from 'react-native';
-import MyStack from './routes';
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MainAction from './pages/main'
+import Info from './pages/info';
+import { NavigationContainer } from '@react-navigation/native';
+const Tab = createBottomTabNavigator();
 
+
+
+
+
+export default function MyTabs() {
+  return (
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={MainAction} />
+      <Tab.Screen name="Info" component={Info} />
+    </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,16 +1,53 @@
-import React, {Component} from 'react';
-import {View, Text } from 'react-native';
 
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
 
+import MapView from 'react-native-maps'
 
-export default class MainAction extends Component {
-    render() {
-        return (
-          <View>
-              <Text>
-                  Caganeira
-              </Text>
-          </View>
-        );
-      }
+export default class App extends Component {
+
+  render() {
+    return (
+
+      <View style={styles.container}>
+
+        <MapView
+          style={styles.map}
+          loadingEnabled={true}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        >
+        
+        </MapView>
+
+      </View>
+
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  
+  container: {
+  
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+
+  },
+
+  map: {
+
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+
+  },
+
+});
